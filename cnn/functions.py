8b6e6be82994efd555b896a8676a4cd95a22406f
Ji Hun Keom <samkeom@korea.ac.kr>
2021-12-29 14:24:26 +0900

    Delete cnn directory

--- cnn/functions.py
+++
@@ -1,8 +0,0 @@
-def rotate_180(m):
-    N = len(m)
-    ret = [[0] * N for _ in range(N)]
-
-    for r in range(N):
-        for c in range(N):
-            ret[N-1-r][N-1-c] = m[r][c]
-    return ret
